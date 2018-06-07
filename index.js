54a37bbe3b43b3d2cdb9f32fb65fe8552e605a28
chnbohwr <chnbohwr@gmail.com>
2018-06-07 14:39:16 +0800

    add containers and styled-components

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Main from './src/containers/Main/Main';
 
-AppRegistry.registerComponent('mopcon_android_app_2018', () => App);
+AppRegistry.registerComponent('mopcon_android_app_2018', () => Main);
+ 
