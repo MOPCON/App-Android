8435e7cf4053b74c0dd0118f51e0d1d5af180d72
chnbohwr <chnbohwr@gmail.com>
2018-06-10 22:44:06 +0800

    adjust fonts and padding

--- src/containers/Schedule/Schedule.js
+++ src/containers/Schedule/Schedule.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native';
+import { ScheduleContainer } from './style';
+import ScheduleHeader from '../../components/ScheduleItem/ScheduleHeader';
+import ScheduleItem from '../../components/ScheduleItem/ScheduleItem';
 
 export default class Schedule extends Component {
   render() {
     return (
-      <View>
-        <Text>this is Schedule page</Text>
-      </View>
+      <ScheduleContainer>
+        <ScheduleHeader time="08:00 ~ 09:00" />
+        <ScheduleItem 
+        title="Innovate width New Technologies on Google" 
+        type="CLOUD" 
+        name="田與折" 
+        room="R1 : 一廳" />
+      </ScheduleContainer>
     )
   }
 }
