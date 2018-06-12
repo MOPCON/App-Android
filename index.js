83f1b3fc35e0767425e7b3972736fadda1dcdd6c
chnbohwr <chnbohwr@gmail.com>
2018-06-12 22:01:44 +0800

    add view to scroll view

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import { AppRegistry } from 'react-native';
 import App from './src/containers/App/App';
 import Schedule from './src/containers/Schedule/Schedule'
 
-AppRegistry.registerComponent('mopcon_android_app_2018', () => Schedule);
+AppRegistry.registerComponent('mopcon_android_app_2018', () => App);
