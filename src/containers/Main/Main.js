87343e8e095057ea7abc549da6b3cb473eb1b24f
Andy Tsai <bbandydd@gmail.com>
2018-06-13 01:17:07 +0800

    add carousel

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -7,9 +7,12 @@ import {
   Image
 } from 'react-native';
 import styled from 'styled-components/native';
+import Carousel from 'react-native-snap-carousel';
 import * as theme from '../../theme';
 import Background from './Background';
 import mopconLogo from '../../images/mopconLogo01.png';
+
+const { height, width } = Dimensions.get('window');
 
 const Container = styled.View`
   flex: 1;
@@ -30,7 +33,30 @@ const LogoContainer = styled.View`
   margin-bottom: 25;
 `;
 
+const CarouselItem = styled.Image`
+  width: 300;
+  height: 168;
+`;
+
 export default class Main extends Component {
+  state = {
+    images: [
+      'https://unsplash.it/300/?random',
+      'https://unsplash.it/350/?random',
+      'https://unsplash.it/400/?random',
+      'https://unsplash.it/450/?random',
+      'https://unsplash.it/500/?random',
+      'https://unsplash.it/550/?random',
+      'https://unsplash.it/600/?random'
+    ],
+  }
+
+  renderItem = ({item, index}) => {
+    return (
+      <CarouselItem  source={{ uri: item }} />
+    );
+  }
+
   render() {
     return (
       <Container>
@@ -39,7 +65,13 @@ export default class Main extends Component {
           <LogoContainer>
             <Image source={mopconLogo}/>
           </LogoContainer>
-          <Text>carousel</Text>
+          <Carousel
+            firstItem={1}
+            sliderWidth={width}
+            itemWidth={300}
+            data={this.state.images}
+            renderItem={this.renderItem}
+          />
         </Content>
       </Container>
     )
