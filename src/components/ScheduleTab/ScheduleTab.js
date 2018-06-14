6b817a011ae94fb899e4f1bc729dbd0cfa90e7b3
chnbohwr <chnbohwr@gmail.com>
2018-06-14 16:17:46 +0800

    refactor scheduleTab

--- src/components/ScheduleTab/ScheduleTab.js
+++ src/components/ScheduleTab/ScheduleTab.js
@@ -1,36 +1,68 @@
 import React, { Component } from 'react';
+import { Animated, Dimensions } from 'react-native';
 import PropTypes from 'prop-types';
 import { TabContainer, TabActiveItem, TouchArea, TextContainer, TabText } from './style';
 
-
 export default class ScheduleTab extends Component {
   static propTypes = {
+    tabs: PropTypes.arrayOf(PropTypes.string),
+    defaultActiveTab: PropTypes.string,
+    onChange: PropTypes.func,
   }
-  state = {
-    isFirstDay: true,
+  static defaultProps = {
+    tabs: ['day1', 'day2', 'day3'],
+    defaultActiveTab: 'day2',
+    onChange: () => { },
   }
-  onPressA = () => {
+  constructor(props) {
+    super(props);
+    const index = props.tabs.findIndex(t => t === props.defaultActiveTab);
+    const length = props.tabs.length;
+    this.screenWidth = Dimensions.get('window').width;
+    this.state = {
+      tabs: [...props.tabs],
+      activeTab: props.defaultActiveTab,
+      activeBarPosition: new Animated.Value(index / length),
+      tabWidth: 100 / length,
+    }
+  }
+  onPressTab = (tab) => {
+    const index = this.state.tabs.findIndex(t => t === tab);
+    const length = this.state.tabs.length;
+    Animated.timing(
+      this.state.activeBarPosition,
+      {
+        toValue: this.compWidth * index / length,
+        duration: 200,
+      }
+    ).start();
     this.setState({
-      isFirstDay: true,
+      activeTab: tab,
     });
+    this.props.onChange(tab);
   }
-  onPressB = () => {
-    this.setState({
-      isFirstDay: false,
-    });
+  onLayout = (e) => {
+    const index = this.state.tabs.findIndex(t => t === this.state.activeTab);
+    const length = this.state.tabs.length;
+    this.compWidth = e.nativeEvent.layout.width;
+    const activeBarPosition = this.compWidth * index / length;
+    if (this.state.activeBarPosition._value !== activeBarPosition) {
+      this.setState({ activeBarPosition: new Animated.Value(activeBarPosition) });
+    }
   }
   render() {
-    const { isFirstDay } = this.state;
+    const { tabs, activeTab, activeBarPosition, tabWidth } = this.state;
     return (
-      <TabContainer>
-        <TabActiveItem isFirstDay={isFirstDay} />
+      <TabContainer onLayout={this.onLayout}>
+        <TabActiveItem tabWidth={tabWidth} style={{ transform: [{ translateX: activeBarPosition }] }} />
         <TextContainer>
-          <TouchArea onPress={this.onPressA}>
-            <TabText isFirstDay={isFirstDay} >11/3</TabText>
-          </TouchArea>
-          <TouchArea onPress={this.onPressB}>
-            <TabText isFirstDay={!isFirstDay}>11/4</TabText>
-          </TouchArea>
+          {
+            tabs.map(tab => (
+              <TouchArea onPress={() => { this.onPressTab(tab) }} key={`tab_${tab}`}>
+                <TabText isActive={tab === activeTab} >{tab}</TabText>
+              </TouchArea>
+            ))
+          }
         </TextContainer>
       </TabContainer>
     )
