41bf329bb788b181013c400a1ca815a60cf72d12
Andy Tsai <bbandydd@gmail.com>
2018-07-03 00:18:41 +0800

    add community detail

--- src/containers/CommunityDetail/CommunityDetail.js
+++ src/containers/CommunityDetail/CommunityDetail.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react';
 import NavigationOptions from '../../components/NavigationOptions/NavigationOptions';
 import * as Style from './style';
 
+import iconFB from '../../images/icon/facebook.png';
+import iconShare from '../../images/icon/group.png';
+
 export default class CommunityDetail extends Component {
   static navigationOptions = ({ navigation }) => NavigationOptions(navigation, 'home.Community', 'mode2')
 
   render() {
     return (
       <Style.Container>
+        <Style.Card/>
+        <Style.Title>KSDG</Style.Title>
+        <Style.Content>
+          Kaohsiung Software Developer Group 是高雄從事軟體開發、網路技術開發者一起交流的園地，2012/05首度聚會活動。
+          目前每月舉辦 Meet-up 及 Web Course 各一場，交流主題涵蓋各種軟體技術，歡迎對軟體開發技術有興趣的朋友可以踴躍參與。
+        </Style.Content>
+        <Style.BtnContainer>
+          <Style.Btn>
+            <Style.BtnImage source={iconFB} />
+          </Style.Btn>
+          <Style.Btn>
+            <Style.BtnImage source={iconShare} />
+          </Style.Btn>
+        </Style.BtnContainer>
       </Style.Container>
     );
   }
--- src/containers/CommunityDetail/style.js
+++ src/containers/CommunityDetail/style.js
@@ -1,7 +1,48 @@
 import styled from 'styled-components/native';
-import { darkBlue } from '../../theme/index';
+import * as theme from '../../theme';
 
 export const Container = styled.View`
-  background-color: ${darkBlue};
+  background-color: ${theme.darkBlue};
   flex: 1;
+  padding: 16px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Card = styled.View`
+  width: 100%;
+  height: 164px;
+  border-radius: 10px;
+  margin-bottom: 24px;
+  background: white;
+`;
+
+export const Title = styled.Text`
+  color: ${theme.scheduleCardTypeColor};
+  font-size: 16px;
+  line-height: 16px;
+  letter-spacing: 2.7px;
+  margin-bottom: 16px;
+`;
+
+export const Content = styled.Text`
+  color: #fff;
+  font-size: 14px;
+  line-height: 22px;
+  letter-spacing: 0.3px;
+  margin-bottom: 32px;
+`;
+
+export const BtnContainer = styled.View`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const Btn = styled.TouchableOpacity`
+  margin-right: 16px;
+`;
+
+export const BtnImage = styled.Image`
 `;
