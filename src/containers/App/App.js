44abcf83243516f7318d40d9c0a9c397b1eb6f83
Andy Tsai <bbandydd@gmail.com>
2018-07-02 23:44:33 +0800

    Main set App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -36,7 +36,7 @@ class App extends Component {
 }
 
 export default StackNavigator({
-  Main: { screen: Community },
+  Main: { screen: App },
   Schedule: { screen: Schedule },
   UnConf: { screen: UnConf },
   ScheduleDetail: { screen: ScheduleDetail },
