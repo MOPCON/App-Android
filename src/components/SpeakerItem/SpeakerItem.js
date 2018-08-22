f9f86d4ca82b651eea403ea2a8307ceefefdc172
chnbohwr <chnbohwr@gmail.com>
2018-08-22 22:38:58 +0800

    connect api with speaker item and schedule detail components

--- src/components/SpeakerItem/SpeakerItem.js
+++ src/components/SpeakerItem/SpeakerItem.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import * as Style from './style';
 
 import { Text } from 'react-native';
 
 export default class SpeakerItem extends Component {
+  static propTypes = {
+    name: PropTypes.string,
+    job: PropTypes.string,
+    info: PropTypes.string,
+    picture: PropTypes.string,
+  }
+  static defaultProps = {
+    name: '',
+    job: '',
+    info: '',
+    picture: '',
+  }
   render() {
+    const { name, job, info } = this.props;
     return (
       <Style.SpeakerItemContainer>
         <Style.ImageContainer>
           <Style.Image></Style.Image>
         </Style.ImageContainer>
         <Style.ContentContainer>
-          <Style.Title>田哲宇</Style.Title>
-          <Style.Info>台灣區經理</Style.Info>
-          <Style.Info>Google Cloud</Style.Info>
+          <Style.Title>{name}</Style.Title>
+          <Style.Info>{job}</Style.Info>
+          <Style.Info>{info}</Style.Info>
         </Style.ContentContainer>
       </Style.SpeakerItemContainer>
     );
--- src/components/SpeechItem/SpeechItem.js
+++ src/components/SpeechItem/SpeechItem.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import * as Style from './style';
 import Button from '../Button/Button';
 
 import buttonStarNormal from '../../images/icon/buttonStarNormal.png';
 import buttonSlide from '../../images/icon/buttonSlide.png';
 
-export default class SpeakerItem extends Component {
+export default class SpeakerItem extends PureComponent {
+  static propTypes = {
+    type: PropTypes.string,
+    topic: PropTypes.string,
+    color: PropTypes.oneOf(['normal', 'inverse'])
+  }
   static defaultProps = {
+    type: '',
+    topic: '',
     color: 'normal'  // normal or inverse
   }
   render() {
-    const { color } = this.props;
-
+    const { color, type, topic } = this.props;
     return (
       <Style.SpeechItemContainer color={color}>
-        <Style.Title>MOBILE APP & ENG</Style.Title>
-        <Style.Content>如何寫出有效率、好讀、好維護的Java / Android 程式</Style.Content>
+        <Style.Title>{type}</Style.Title>
+        <Style.Content>{topic}</Style.Content>
         <Style.FuncView>
           <Button text="加入行程" margin={[0, 8, 0, 0]} iconURI={buttonStarNormal} color="inverse" />
           <Button text="投影片" margin={[0, 0, 0, 0]} iconURI={buttonSlide} color="inverse" />
