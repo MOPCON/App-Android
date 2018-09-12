858b72a936345c5b6ad5814d9bc55e745a21f45a
Andy Tsai <bbandydd@gmail.com>
2018-09-12 10:31:43 +0800

    remove fake items

--- src/containers/Community/VolunteerBlock.js
+++ src/containers/Community/VolunteerBlock.js
@@ -26,57 +26,6 @@ export default class VolunteerBlock extends Component {
             </Style.Card>
           ))
         }
-        
-        <Style.Card>
-          <Style.CardTitle>議程委員會</Style.CardTitle>
-          <Style.CardContent>
-            議程品質的守門員，審核各方來稿是否適合 MOPCON 舞台。
-            </Style.CardContent>
-          <Style.MemberTitle>委員名單</Style.MemberTitle>
-          <Style.MemberContent>
-            Ant, DK, Howard, John Chang, KNY, 良葛格, Sam Lu
-            </Style.MemberContent>
-        </Style.Card>
-        <Style.Card>
-          <Style.CardTitle>議程委員會</Style.CardTitle>
-          <Style.CardContent>
-            議程品質的守門員，審核各方來稿是否適合 MOPCON 舞台。
-            </Style.CardContent>
-          <Style.MemberTitle>委員名單</Style.MemberTitle>
-          <Style.MemberContent>
-            Ant, DK, Howard, John Chang, KNY, 良葛格, Sam Lu
-            </Style.MemberContent>
-        </Style.Card>
-        <Style.Card>
-          <Style.CardTitle>議程委員會</Style.CardTitle>
-          <Style.CardContent>
-            議程品質的守門員，審核各方來稿是否適合 MOPCON 舞台。
-            </Style.CardContent>
-          <Style.MemberTitle>委員名單</Style.MemberTitle>
-          <Style.MemberContent>
-            Ant, DK, Howard, John Chang, KNY, 良葛格, Sam Lu
-            </Style.MemberContent>
-        </Style.Card>
-        <Style.Card>
-          <Style.CardTitle>議程委員會</Style.CardTitle>
-          <Style.CardContent>
-            議程品質的守門員，審核各方來稿是否適合 MOPCON 舞台。
-            </Style.CardContent>
-          <Style.MemberTitle>委員名單</Style.MemberTitle>
-          <Style.MemberContent>
-            Ant, DK, Howard, John Chang, KNY, 良葛格, Sam Lu
-            </Style.MemberContent>
-        </Style.Card>
-        <Style.Card>
-          <Style.CardTitle>議程委員會</Style.CardTitle>
-          <Style.CardContent>
-            議程品質的守門員，審核各方來稿是否適合 MOPCON 舞台。
-            </Style.CardContent>
-          <Style.MemberTitle>委員名單</Style.MemberTitle>
-          <Style.MemberContent>
-            Ant, DK, Howard, John Chang, KNY, 良葛格, Sam Lu
-            </Style.MemberContent>
-        </Style.Card>
       </Style.BlockContainer>
     );
   }
