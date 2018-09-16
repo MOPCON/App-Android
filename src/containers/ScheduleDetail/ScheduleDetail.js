2ee972f3d3e34fc91e35b046fa425fc831e848a2
chnbohwr <chnbohwr@gmail.com>
2018-09-16 17:33:22 +0800

    fix schedule detail save func

--- src/containers/ScheduleDetail/ScheduleDetail.js
+++ src/containers/ScheduleDetail/ScheduleDetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import I18n from '../../locales';
-import { ScrollView } from 'react-native';
+import { ScrollView, AsyncStorage } from 'react-native';
 import * as Style from './style';
 import NavigationOptions from '../../components/NavigationOptions/NavigationOptions';
 import SpeechItem from '../../components/SpeechItem/SpeechItem';
@@ -8,21 +8,45 @@ import SpeakerItem from '../../components/SpeakerItem/SpeakerItem';
 
 export default class ScheduleDetail extends React.Component {
   static navigationOptions = ({ navigation }) => NavigationOptions(navigation, 'scheduleDetail.title', 'mode2')
+
+  state = {
+    savedSchedule: {},
+  }
+
+  async componentDidMount() {
+    const savedScheduleText = await AsyncStorage.getItem('savedschedule');
+    let savedSchedule = JSON.parse(savedScheduleText);
+    if (!savedSchedule) { savedSchedule = {}; }
+    this.setState({
+      savedSchedule,
+    });
+  }
+
+  onSave = (schedule_id) => () => {
+    const savedSchedule = {
+      ...this.state.savedSchedule,
+    };
+    savedSchedule[schedule_id] = !savedSchedule[schedule_id];
+    this.setState({ savedSchedule });
+    AsyncStorage.setItem('savedschedule', JSON.stringify(savedSchedule));
+  }
+
   render() {
     const { navigation } = this.props;
+    const { savedSchedule } = this.state;
     const lang = I18n.locale;
     const agenda = navigation.getParam('agenda', {});
-    console.log(agenda);
     return (
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
         <Style.SDContainer>
-          <Style.HeaderImage source={{ uri: 'https://picsum.photos/700/1000/?image=1063' }} />
           <Style.IntroContainer>
             <Style.SpeechItemContainer>
               <SpeechItem
                 color="inverse"
                 type={agenda.type}
                 topic={lang === 'zh' ? agenda.schedule_topic : agenda.schedule_topic_en}
+                saved={savedSchedule[agenda.schedule_id]}
+                onSave={this.onSave(agenda.schedule_id)}
               />
             </Style.SpeechItemContainer>
             <Style.DesText>
