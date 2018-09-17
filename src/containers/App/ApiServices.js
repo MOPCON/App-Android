e89195e3fd65f28526a3246ed7f56776752c7133
Andy Tsai <bbandydd@gmail.com>
2018-09-17 23:05:55 +0800

    add news

--- src/containers/App/ApiServices.js
+++ src/containers/App/ApiServices.js
@@ -6,11 +6,12 @@ const sponsor = fetch(`${process.env.MOPCON_API_URL}/sponsor`);
 const community = fetch(`${process.env.MOPCON_API_URL}/community`);
 const volunteer = fetch(`${process.env.MOPCON_API_URL}/volunteer`);
 const carousel = fetch(`${process.env.MOPCON_API_URL}/carousel`);
+const news = fetch(`${process.env.MOPCON_API_URL}/news`);
 
 export const reportApiError = () => {
   // report api error to server;
 }
 
-export const updateData = () => Promise.all([schedule, codeOfConduct, speaker, unconf, sponsor, community, volunteer, carousel])
+export const updateData = () => Promise.all([schedule, codeOfConduct, speaker, unconf, sponsor, community, volunteer, carousel, news])
 .then(responseList => responseList.map(response => response.text()))
 .then(promiseList => Promise.all(promiseList));
