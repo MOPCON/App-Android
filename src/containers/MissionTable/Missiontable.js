80bd341acbcf96ef83c072a8a2723093519f494d
Andy Tsai <bbandydd@gmail.com>
2018-09-27 01:03:18 +0800

    get-quiz api

--- src/containers/MissionTable/Missiontable.js
+++ src/containers/MissionTable/Missiontable.js
@@ -6,11 +6,41 @@ import NavigationOptions from '../../components/NavigationOptions/NavigationOpti
 import Exchange from './Exchange';
 import Mission from './Mission';
 import Mask from './Mask';
+import apiServices from '../../api/services';
 
 export default class MissionTable extends Component {
   static navigationOptions = ({ navigation }) => NavigationOptions(navigation, 'missionTable.title', 'mode1')
 
+  state = {
+    quizs: [],
+  }
+
+  async componentDidMount() {
+    const result = await apiServices.get('/get-quiz');
+
+    // 之後格式會改
+    const quizs = result.reduce((acc, val) => [...acc, ...(val.items || [])], []);
+
+    /*
+      status: (按照此順序)
+      1 尚未挑戰
+      0 尚未開放挑戰
+      2 挑戰成功
+      -1 挑戰失敗
+    */
+    this.setState({
+      quizs: [
+        ...quizs.filter(o => o.status === '1'),
+        ...quizs.filter(o => o.status === '0'),
+        ...quizs.filter(o => o.status === '2'),
+        ...quizs.filter(o => o.status === '-1'),
+      ]
+    });
+  }
+
   render() {
+    const { quizs } = this.state;
+
     return (
       <Style.MissionContainer>
         <Style.ScrollContainer>
@@ -21,26 +51,32 @@ export default class MissionTable extends Component {
               </Style.Box>
             </Style.ExchangeZone>
             <Style.MissionZone>
-              <Style.Box>
-                <Mission isLocked title={I18n.t('missionTable.unlock')} content="01:22:03"></Mission>
-              </Style.Box>
-              <Style.Box disabled>
-                <Mission title="Q&A" content="找到紫色小鴨"></Mission>
-                <Mask status="0"/>
-              </Style.Box>
-               <Style.Box disabled>
-                <Mission title="Q&A" content="找到紫色小鴨"></Mission>
-                <Mask status="1"/>
-              </Style.Box>
-               <Style.Box>
-                <Mission title="Q&A" content="找到紫色小鴨"></Mission>
-              </Style.Box>
-               <Style.Box>
-                <Mission title="Q&A" content="找到紫色小鴨"></Mission>
-              </Style.Box>
-               <Style.Box>
-                <Mission title="Q&A" content="找到紫色小鴨"></Mission>
-              </Style.Box>
+              {
+                quizs.map((quiz, index) => {
+                  const { status, type, title } = quiz;
+
+                  if (status === '0') { // 尚未開放挑戰
+                    return (
+                      <Style.Box key={`quiz_${index}`}>
+                        <Mission isLocked title={I18n.t('missionTable.unlock')} content="01:22:03"></Mission>
+                      </Style.Box>
+                    );
+                  }
+
+                  const disabled = (status === '2' || status === '-1');
+
+                  return (
+                    <Style.Box key={`quiz_${index}`} disabled={disabled}>
+                      <Mission title="Q&A" content={title}></Mission>
+                      {
+                        disabled && (
+                          <Mask status={status} />
+                        )
+                      }
+                    </Style.Box>
+                  );
+                })
+              }
             </Style.MissionZone>
           </View>
         </Style.ScrollContainer>
