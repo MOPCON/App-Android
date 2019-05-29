dddb8a5deda1bae54c7a85581a2edcd00db6e739
Andy Tsai <bbandydd@gmail.com>
2019-05-29 22:49:50 +0800

    add splash screen

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -6,7 +6,7 @@ import {
 } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import Carousel from 'react-native-snap-carousel';
-// import SplashScreen from 'react-native-splash-screen';
+import SplashScreen from 'react-native-splash-screen';
 import I18n from '../../locales';
 
 // component
@@ -76,7 +76,7 @@ export default class Main extends Component {
       news: JSON.parse(newsText).payload,
     });
 
-    // SplashScreen.hide();
+    SplashScreen.hide();
   }
 
   openLink = (url) => () => {
