aa9ba1db7280bbcddf701fbd3da639b144a32b67
chnbohwr <chnbohwr@gmail.com>
2019-07-18 22:40:09 +0800

    schedule header and item layout change to 2019 style

--- src/components/ScheduleItem/ScheduleItem.js
+++ src/components/ScheduleItem/ScheduleItem.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import * as Style from './style';
-import { TouchableOpacity } from 'react-native';
 import geoPng from '../../images/location.png';
-import starIconNormal from '../../images/buttonStarNormal.png';
-import starIconChecked from '../../images/buttonStarChecked.png';
+
 
 export default class ScheduleItem extends Component {
   static propTypes = {
@@ -13,14 +11,8 @@ export default class ScheduleItem extends Component {
     category: PropTypes.string,   // topic category string eg. CLOUD
     name: PropTypes.string,   // speaker name
     room: PropTypes.string,   // room location string
-    saved: PropTypes.bool,    // saved or not
-    onSave: PropTypes.func,   // when click on save button trigger this function.
     regular: PropTypes.bool,  // regular conference schedule or not
     onPressTitle: PropTypes.func,
-  }
-
-  onPressSave = () => {
-    this.props.onSave();
   }
 
   onPressTitle = () => {
@@ -28,29 +20,23 @@ export default class ScheduleItem extends Component {
   }
 
   render() {
-    const { paintBG, title, category, name, room, saved, regular } = this.props;
+    const { paintBG, title, category, name, room, regular } = this.props;
     return (
       <Style.ScheduleItemContainer paintBG={paintBG}>
         {
           paintBG ? <Style.Title paintBG={paintBG}>{title}</Style.Title> : (
             <Style.InnerContainer>
-              <Style.Category>{category}</Style.Category>
               <Style.TitleTouchable onPress={this.onPressTitle}>
                 <Style.Title paintBG={paintBG}>{title}</Style.Title>
               </Style.TitleTouchable>
-              <Style.Name>{name}</Style.Name>
               <Style.ActionContainer>
+                <Style.Name>{name}</Style.Name>
                 {
-                  // <Style.GeoContainer>
-                  //   <Style.GeoIcon source={geoPng} />
-                  //   <Style.Room>{room}</Style.Room>
-                  // </Style.GeoContainer>
-                }
-                {
-                  regular && (
-                    <Style.StarIconTouchable onPress={this.onPressSave}>
-                      <Style.StarIconImg source={saved ? starIconChecked : starIconNormal} />
-                    </Style.StarIconTouchable>
+                  room && (
+                    <Style.GeoContainer>
+                      <Style.GeoIcon source={geoPng} />
+                      <Style.Room>{room}</Style.Room>
+                    </Style.GeoContainer>
                   )
                 }
               </Style.ActionContainer>
@@ -60,3 +46,13 @@ export default class ScheduleItem extends Component {
     )
   }
 }
+
+
+// <Style.Category>{category}</Style.Category>
+// {
+//   regular && (
+//     <Style.StarIconTouchable onPress={this.onPressSave}>
+//       <Style.StarIconImg source={saved ? starIconChecked : starIconNormal} />
+//     </Style.StarIconTouchable>
+//   )
+// }
