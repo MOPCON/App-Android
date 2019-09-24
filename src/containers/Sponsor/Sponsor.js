962a43e50500b662d7b2d214e52dbed08569f3ea
chnbohwr <chnbohwr@gmail.com>
2019-09-24 21:49:53 +0800

    sponsor loading status

--- src/containers/Sponsor/Sponsor.js
+++ src/containers/Sponsor/Sponsor.js
@@ -1,22 +1,11 @@
 import React, { Fragment } from 'react';
-import { ScrollView, Text } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+import { ScrollView } from 'react-native';
 import I18n from '../../locales';
 import * as Style from './style';
 import NavigationOptions from '../../components/NavigationOptions/NavigationOptions';
-import { objectTypeAnnotation } from '@babel/types';
+import LoadingIcon from '../../components/LoadingIcon/LoadingIcon';
 import apiServices from '../../api/services';
 
-const TYPE = {
-  TS: { name: 'Tony Stark', id: 'tony_stark' },
-  BW: { name: 'Bruce Wayne', id: 'bruce_wayne' },
-  HACKER: { name: 'Hacker', id: 'hacker' },
-  GEEK: { name: 'Geek', id: 'geek' },
-  DEV: { name: 'Developer', id: 'developer' },
-  HLP: { name: '協辦單位', id: 'special_cooperation' },
-  TXS: { name: '特別感謝', id: 'special_thanks' },
-  TUT: { name: '教育贊助', id: 'education' },
-}
 
 export default class Sponsor extends React.Component {
   static navigationOptions = ({ navigation }) => NavigationOptions(navigation, 'sponsor.title', 'mode2')
@@ -41,15 +30,17 @@ export default class Sponsor extends React.Component {
   }
 
   render() {
-    const { sponsor, language } = this.state;
+    const { sponsor } = this.state;
+    const lang = I18n.locale;
     return (
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
         <Style.SponsorContainer style={{ flexGrow: 1 }}>
           {
-            sponsor.map(sponsorData => (
-              <Fragment key={sponsorData.name_e}>
-                <Style.TypeText>{I18n.locale === 'en' ? sponsorData.name_e : sponsorData.name}</Style.TypeText>
-                <Style.CardSmallView>
+            sponsor.length
+              ? sponsor.map(sponsorData => (
+                <Fragment key={sponsorData.name_e}>
+                  <Style.TypeText>{lang === 'zh' ? sponsorData.name : sponsorData.name_e}</Style.TypeText>
+                  <Style.CardSmallView>
                     {
                       sponsorData.data.map(sponsorDetail => (
                         <Style.CardSmall onPress={this.onClickImage(sponsorDetail)} key={`sponsor${sponsorDetail.name_e}`}>
@@ -57,38 +48,18 @@ export default class Sponsor extends React.Component {
                             <Style.CardImg source={{ uri: sponsorDetail.logo_path }} />
                           </Style.CardImgSmall>
                           <Style.CardText>
-                            {language === 'en' ? sponsorDetail.name_e : sponsorDetail.name}
+                            {lang === 'zh' ? sponsorDetail.name : sponsorDetail.name_e}
                           </Style.CardText>
                         </Style.CardSmall>
                       ))
                     }
                   </Style.CardSmallView>
-              </Fragment>
-            ))
+                </Fragment>
+              ))
+              : <LoadingIcon size="large" color="#ffffff" />
           }
         </Style.SponsorContainer>
       </ScrollView>
     );
   }
 }
-
-
-
-
-/* <Fragment key={TYPE[key].id}>
-                  <Style.TypeText>{TYPE[key].name}</Style.TypeText>
-                  <Style.CardSmallView>
-                    {
-                      sponsor[TYPE[key].id].map(sponsorData => (
-                        <Style.CardSmall onPress={this.onClickImage(sponsorData)} key={`sponsor${sponsorData.sponsor_en}`}>
-                          <Style.CardImgSmall>
-                            <Style.CardImg source={{ uri: sponsorData.logo_path }} />
-                          </Style.CardImgSmall>
-                          <Style.CardText>
-                            {language === 'zh' ? sponsorData.sponsor : sponsorData.sponsor_en}
-                          </Style.CardText>
-                        </Style.CardSmall>
-                      ))
-                    }
-                  </Style.CardSmallView>
-                </Fragment> */
