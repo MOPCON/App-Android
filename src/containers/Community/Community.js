2460def43341329db4ac2ef28cbfc4bd8d558847
chnbohwr <chnbohwr@gmail.com>
2019-09-24 21:57:07 +0800

    community loading status

--- src/containers/Community/Community.js
+++ src/containers/Community/Community.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { ScrollView, Linking } from 'react-native';
+import { Linking } from 'react-native';
 import VersionNum from 'react-native-version-number';
-import AsyncStorage from '@react-native-community/async-storage';
 import apiServices from '../../api/services'
 import * as Style from './style';
 import I18n from '../../locales';
@@ -9,6 +8,7 @@ import NavigationOptions from '../../components/NavigationOptions/NavigationOpti
 import TabDate from '../../components/TabDate/TabDate';
 import CommunityBlock from './CommunityBlock';
 import VolunteerBlock from './VolunteerBlock';
+import LoadingIcon from '../../components/LoadingIcon/LoadingIcon';
 
 import iconJoin from '../../images/icon/iconJoin.png';
 import iconFollowFB from '../../images/icon/iconFollowFB.png';
@@ -24,9 +24,13 @@ export default class Community extends Component {
   }
 
   getData = async () => {
-    const [{ data: { community, participant } }, { data: { volunteer } }] = await Promise.all([apiServices.get('/community'), apiServices.get('/volunteer')])
-    // const { data: { community, participant } } = await apiServices.get('/community');
-    // const { data: { volunteer } } = await apiServices.get('/volunteer');
+    const [
+      { data: { community, participant } },
+      { data: { volunteer } }
+    ] = await Promise.all([
+      apiServices.get('/community'),
+      apiServices.get('/volunteer')
+    ]);
     this.setState({ community, volunteer, participant });
   }
 
@@ -61,29 +65,37 @@ export default class Community extends Component {
     ];
 
     return (
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <Style.Container>
-          <Style.TabContainer>
-            <TabDate tabs={tabs} defaultActiveTab={tab} onChange={this.handleChange} />
-          </Style.TabContainer>
-          {
-            tab === 'community'
-              ? <CommunityBlock goCommunityDetail={this.goCommunityDetail} community={community} participant={participant} />
-              : <VolunteerBlock goVolunteerDetail={this.goVolunteerDetail} volunteer={volunteer} />
-          }
-        </Style.Container>
-        <Style.JoinContainer>
-          <Style.JoinImage source={iconJoin} />
-          <Style.JoinText>「我想加入志工行列！」</Style.JoinText>
-          <Style.FollowView onPress={this.goFB}>
-            <Style.FollowImage source={iconFollowFB} />
-          </Style.FollowView>
-          <Style.FollowText>
-            想要和我們一起改變南部資訊生態圈嗎？歡迎追蹤我們的 Facebook，我們會在下一屆準備開始前 PO 出徵才資訊！加入我們不僅有機會參與改變的過程，還可以得到寶貴的辦展經驗，認識大神們哦！
-          </Style.FollowText>
-          <Style.VersionText>{`Ver: ${VersionNum.appVersion}-${VersionNum.buildVersion}`}</Style.VersionText>
-        </Style.JoinContainer>
-      </ScrollView>
+      <Style.ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        {
+          Boolean(community.length)
+            ? (
+              <React.Fragment>
+                <Style.Container>
+                  <Style.TabContainer>
+                    <TabDate tabs={tabs} defaultActiveTab={tab} onChange={this.handleChange} />
+                  </Style.TabContainer>
+                  {
+                    tab === 'community'
+                      ? <CommunityBlock goCommunityDetail={this.goCommunityDetail} community={community} participant={participant} />
+                      : <VolunteerBlock goVolunteerDetail={this.goVolunteerDetail} volunteer={volunteer} />
+                  }
+                </Style.Container>
+                <Style.JoinContainer>
+                  <Style.JoinImage source={iconJoin} />
+                  <Style.JoinText>「我想加入志工行列！」</Style.JoinText>
+                  <Style.FollowView onPress={this.goFB}>
+                    <Style.FollowImage source={iconFollowFB} />
+                  </Style.FollowView>
+                  <Style.FollowText>
+                    想要和我們一起改變南部資訊生態圈嗎？歡迎追蹤我們的 Facebook，我們會在下一屆準備開始前 PO 出徵才資訊！加入我們不僅有機會參與改變的過程，還可以得到寶貴的辦展經驗，認識大神們哦！
+                  </Style.FollowText>
+                  <Style.VersionText>{`Ver: ${VersionNum.appVersion}-${VersionNum.buildVersion}`}</Style.VersionText>
+                </Style.JoinContainer>
+              </React.Fragment>
+            )
+            : (<LoadingIcon size="large" color="#ffffff" />)
+        }
+      </Style.ScrollView>
     );
   }
 }
