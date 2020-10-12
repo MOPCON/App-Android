034b1e8a8d7e5a57b596a33389df0fd6e1ff48fa
Hinrick <hinrick71@gmail.com>
2020-10-12 08:08:12 +0800

    fetch from develop

--- src/containers/Game/GameV2.js
+++ src/containers/Game/GameV2.js
@@ -192,7 +192,7 @@ const Game = ({ navigation, context }) => {
     }
   }
 
-  const { modalWelcomeVisible, modalRewardVisible, intro, reward, isLoading, missions, progressLeft, progressRight, passed} = state;
+  const { modalWelcomeVisible, modalRewardVisible, intro, reward, isLoading, missions} = state;
 
   // const { score, missionList, isLoaded, lastPassIndex } = context.gameStore;
 
@@ -211,7 +211,7 @@ const Game = ({ navigation, context }) => {
                   <Style.UserIcon source={avatarUser} />
                   <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
                     <Style.TotalText>{I18n.t('game.total_score')}</Style.TotalText>
-                    <Style.ScoreText>{passed}/12</Style.ScoreText>
+                    <Style.ScoreText>{12}/12</Style.ScoreText>
                   </View>
                   {/** 進度條 */}
                   <Style.ProgressContainer>
@@ -223,8 +223,8 @@ const Game = ({ navigation, context }) => {
                       <Style.ProgressGift source={inActiveIcon}/>
                     </View>
                     <Style.ProgressBar>
-                      <View style={{width:`${progressRight}%`, height: 4, backgroundColor:'#ffcc00'}}></View>
-                      <View style={{width:`${100-progressRight}%`, height: 4, backgroundColor:'#fff'}}></View>
+                      <View style={{width:`${progressRight}%`, height: 4, backgroundColor:'#fff'}}></View>
+                      <View style={{width:`${100-progressRight}%`, height: 4, backgroundColor:'#ffcc00'}}></View>
                     </Style.ProgressBar>
                     <View style={{width:'10%'}}>
                       <Style.ProgressGift source={inActiveIcon}/>
