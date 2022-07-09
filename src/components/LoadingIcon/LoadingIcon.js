aede62bc003744b778539f27ca76e19cb7590e5f
Cheryl <cherylgao@17.media>
2022-07-09 14:16:17 +0800

    init project. refector to native android.

--- src/components/LoadingIcon/LoadingIcon.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components/native';
-const LoadingIcon = styled.ActivityIndicator`
-  width: 100%;
-  height: 100%;
-`;
-
-export default LoadingIcon;
--- src/containers/QRCode/style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native';
-import {
-    darkBlue,
-  } from '../../theme/index';
-
-export const QRCodeContainer = styled.View`
-  background-color: ${darkBlue};
-  flex-grow:1;
-`;
